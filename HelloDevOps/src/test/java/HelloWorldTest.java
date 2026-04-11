0bde220a03776258780a140682322582d627a9c9
RazrtroN <tiwari.devansh2002@gmail.com>
2026-04-11 22:38:56 +0530

    DevOps Project

---
+++ HelloDevOps/src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHello() {
+        assertEquals("Hello DevOps", HelloWorld.sayHello());
+    }
+}
